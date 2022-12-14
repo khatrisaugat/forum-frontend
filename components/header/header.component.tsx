e96447d9b1d28ef955843662e45e8ce5cd48d865
khatrisaugat <khatri.saugat55@gmail.com>
2022-12-14 21:41:00 +0545

    tailwind and folder structure for components

---
+++ components/header/header.component.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="container w-screen ">
+      <div className="w-screen mx-auto bg-black h-14"></div>
+    </header>
+  );
+}
+
+export default Header;
