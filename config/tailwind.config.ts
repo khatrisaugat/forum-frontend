c3473c48c59a681909947daa0b6ae96864c30347
khatrisaugat <khatri.saugat55@gmail.com>
2022-12-14 20:27:59 +0545

    initial setup

---
+++ config/tailwind.config.ts
@@ -0,0 +1,13 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/ui")],
+};
